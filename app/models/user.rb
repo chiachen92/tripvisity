af750b1062f7c3fa7bd572b596b78684171a4499
John Doe <chiachen1225@gmail.com>
2016-12-13 09:17:35 -0800

    website so far

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,20 @@ class User < ApplicationRecord
 
   has_many :votes, dependent: :destroy
   has_many :voted_comments, through: :votes, source: :comment
-  
+
+  def self.from_omniauth(auth)
+  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.name = auth.info.name
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user.save!
+  end
+ end
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,8 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
-  # match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  # match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  # match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
 end
