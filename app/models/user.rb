8aea0d722b06277f71eaf49794300e7024306553
John Doe <chiachen1225@gmail.com>
2017-01-27 14:02:25 -0800

    fixing sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   before_save :downcase_email
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, length: {maximum: 30}, format:  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, unless: :from_oauth?
+  validates :email, presence: true, uniqueness: true, length: {maximum: 30}, format:  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   has_many :votes, dependent: :destroy
   has_many :voted_comments, through: :votes, source: :comment
