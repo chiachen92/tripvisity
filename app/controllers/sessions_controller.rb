e97f679f3dcae1fcec1d4e93caa9b03ac2993d3f
John Doe <chiachen1225@gmail.com>
2017-01-30 15:18:20 -0800

    alert messages working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
       @user = User.find_by_email params[:email]
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
-        flash[:success] = 'Signed In'
-        redirect_to root_path
+        redirect_to root_path, notice: "Successfully signed in!"
       else
         flash.now[:danger] = 'Wrong credentials'
         render :new
@@ -18,7 +17,7 @@ class SessionsController < ApplicationController
 
     def destroy
       session[:user_id] = nil
-      flash[:success] = "Signed out"
+      flash[:notice] = "Signed out"
       redirect_to root_path
     end
   end
